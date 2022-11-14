8b405007c79e49e5fb25919bb93bb80f30fe1665
ching <mschingcheng@gmail.com>
2022-11-14 11:52:55 -0800

    problem 20

--- problems/problem_020.py
+++ problems/problem_020.py
@@ -3,4 +3,14 @@
 # to 50% of the number of people in the members list.
 
 def has_quorum(attendees_list, members_list):
-    pass
+    members_count = 0
+    for mem in members_list:
+        members_count += 1
+    attendees_count = 0
+    for attend in attendees_list:
+        attendees_count += 1
+    members_fifty = members_count/2
+    if attendees_count >= members_fifty:
+        return True
+    else:
+        return False
