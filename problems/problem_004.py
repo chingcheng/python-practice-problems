f5a0d1b6fd813d0b8752e8133512714d029b5dc2
ching <mschingcheng@gmail.com>
2022-11-14 10:37:55 -0800

    problem 4

--- problems/problem_004.py
+++ problems/problem_004.py
@@ -13,4 +13,11 @@
 # problem to get a good feel for how to solve it.
 
 def max_of_three(value1, value2, value3):
-    pass
+    if value1 >= value2 and value1 >= value3:
+        return value1
+    elif value2 >= value1 and value2 >= value3:
+        return value2
+    elif value3 >= value1 and value3 >= value2:
+        return value3
+    else:
+        return value1 or value2 or value3
