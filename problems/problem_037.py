617aed84d303525b3911396329a93855b3f9dc39
ching <mschingcheng@gmail.com>
2022-11-15 12:05:59 -0800

    problem 37

--- problems/problem_037.py
+++ problems/problem_037.py
@@ -24,4 +24,10 @@
 #     result: "   19"
 
 def pad_left(number, length, pad):
-    pass
+    # turn number into a string
+    str_number = str(number)
+    # while str_number is less than the desired length
+    while len(str_number) < length:
+        # add pad to str_number
+        str_number = pad + str_number
+    return str_number
