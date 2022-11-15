a42b69948ebd9adfb358b3ac2d35dff3b54f6ea8
ching <mschingcheng@gmail.com>
2022-11-15 14:13:54 -0800

    problem 42

--- problems/problem_042.py
+++ problems/problem_042.py
@@ -13,4 +13,14 @@
 # Look up the zip function to help you with this problem.
 
 def pairwise_add(list1, list2):
-    pass
+    # create result list
+    result = []
+    # use zip to combine list1 and list2 into a tuple
+    joined = zip(list1, list2)
+    # for each pair of numbers in the tuple
+    for pair in joined:
+        # use sum to add the two numbers in each pair
+        pair_sum = sum(pair)
+        # append each sum to the result
+        result.append(pair_sum)
+    return result
