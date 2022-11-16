4346164129c1ef400c8573d0a7ca5447910d4b2a
ching <mschingcheng@gmail.com>
2022-11-15 18:03:40 -0800

    problem 50

--- problems/problem_050.py
+++ problems/problem_050.py
@@ -12,3 +12,16 @@
 #    * input: [1, 2, 3]
 #      result: [1, 2], [3]
 
+def halve_the_list(list):
+    list1 = []
+    list2 = []
+    length = len(list)
+    half_length = length//2 + (len(list)%2)
+
+    for i in range(half_length):
+        list1.append(list[i])
+    for j in range(length//2):
+        index = j + half_length
+        list2.append(list[index])
+
+    return list1, list2
