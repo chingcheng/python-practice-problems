821523ad13ce1d6189f72bc4082331225f9ade10
ching <mschingcheng@gmail.com>
2022-11-16 12:40:55 -0800

    problem 72

--- problems/problem_072.py
+++ problems/problem_072.py
@@ -27,4 +27,17 @@
 # Do it without pseudocode, this time, from memory. Don't look
 # at the last one you just wrote unless you really must.
 
+class Person:
 
+    def __init__ (self, name, hated_foods, loved_foods):
+        self.name = name
+        self.hated_foods = hated_foods
+        self.loved_foods = loved_foods
+
+    def taste(self, food):
+        if food in self.hated_foods:
+            return False
+        elif food in self.loved_foods:
+            return True
+        else:
+            return None
