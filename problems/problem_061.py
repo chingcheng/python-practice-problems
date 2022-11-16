5c35944e606e4c7663276a88bc30f74a9b2a56ca
ching <mschingcheng@gmail.com>
2022-11-15 18:31:28 -0800

    problem 61

--- problems/problem_061.py
+++ problems/problem_061.py
@@ -14,3 +14,11 @@
 #     * input:   [1, 3, 3, 20, 3, 2, 2]
 #       returns: [1, 3, 20, 2]
 
+def remove_duplicates(values):
+    result = []
+
+    for i in values:
+        if i not in result:
+            result.append(i)
+
+    return result
