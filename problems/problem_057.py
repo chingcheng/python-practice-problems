c2cefaffdad4579d1a0e1d53bf46ec22ae2e67fd
ching <mschingcheng@gmail.com>
2022-11-15 16:30:34 -0800

    problem 57

--- problems/problem_057.py
+++ problems/problem_057.py
@@ -13,3 +13,8 @@
 #     * input:   3
 #       returns: 1/2 + 2/3 + 3/4
 
+def sum_fraction_sequence(num):
+    result = 0
+    for num in range(1, num + 1):
+        result += num / (num + 1)
+    return result
